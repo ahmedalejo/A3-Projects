4cb6ff30f6636ba15aa48e4365c2b94f455eb5ac
Ahmed A. Alejo <adebunky@gmail.com>
2014-08-04 19:48:05 -0300

    [GIT-COPY] Initial (Technical) Design Model Project
    With enfase on project structure/organization, BDD via specflow, ORM-Via
    EntityFrameWork, Non-blocking UI, ... e.t.c
    
    Basically it´s mean´t to be a functional Show case of various top .Net
    Technologies, patterns.
    
    Obs: The entire project was started based on the .feature file.
    All other functionalities were consequent.
    
    The project is currently functional!
    
    Comments and specifications are (at the moment) in portuguese. I intend
    to set up an efetive model for multi-lingual specification structure for
    specflow; there by creating a mapped english specification version.

---
+++ RestaurantApp/RestaurantApp.Voting.AcceptanceTests/Steps/BaseSteps.cs
@@ -0,0 +1,5 @@
+﻿using System;
+
+namespace RestaurantApp.Voting.AcceptanceTests.Steps
+{
+}
---
+++ RestaurantApp/RestaurantApp.Voting.AcceptanceTests/Steps/PollClosingSteps.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace RestaurantApp.Voting.AcceptanceTests.Steps
+{
+    [Binding]
+    public class PollClosingSteps
+    {
+        
+        protected VotingTestContext VotingTestContext { get; set; }
+        protected PollClosingSteps(VotingTestContext context)
+        {
+            VotingTestContext = context;
+        }
+
+        [Given(@"que o restaurante '(.*)' já foi escolhido '(.*)'")]
+        public async Task DadoQueORestauranteJaFoiEscolhido(string restaurantId, DateTime date)
+        {
+            var restaurant = await VotingTestContext.Db.Restaurants.FirstAsync(r => r.Name == restaurantId);
+            //restaurant.
+        }
+    }
+}
